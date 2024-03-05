37b69429033af2404e1e930cdced5d636ffa8e82
Jeru Kim <9653327+jerukim@users.noreply.github.com>
2024-03-05 17:07:47 -0500

    bookshelf app tests

--- src/__tests__/index.js
+++ src/__tests__/index.js
@@ -1,21 +1,37 @@
-import {screen, act} from '@testing-library/react'
+import React from 'react'
+import {screen, render} from '@testing-library/react'
+import App from 'App'
+import userEvent from '@testing-library/user-event'
 
 test('renders the app', () => {
-  const root = document.createElement('div')
-  root.id = 'root'
-  document.body.append(root)
-
-  let reactRoot
-  act(() => {
-    reactRoot = require('..').root
-  })
+  render(<App />)
 
   screen.getByTitle('Bookshelf')
   screen.getByRole('heading', {name: /Bookshelf/i})
   screen.getByRole('button', {name: /Login/i})
   screen.getByRole('button', {name: /Register/i})
+})
 
-  // cleanup
-  act(() => reactRoot.unmount())
-  document.body.removeChild(root)
+test('clicking login button opens login dialog', async () => {
+  render(<App />)
+
+  const button = screen.getByRole('button', {name: /Login/i})
+
+  expect(screen.queryByRole('dialog', {name: /login form/i})).toBeNull()
+
+  await userEvent.click(button)
+
+  screen.getByRole('dialog', {name: /login form/i})
+})
+
+test('clicking register button opens register dialog', async () => {
+  render(<App />)
+
+  const button = screen.getByRole('button', {name: /Register/i})
+
+  expect(screen.queryByRole('dialog', {name: /register form/i})).toBeNull()
+
+  await userEvent.click(button)
+
+  screen.getByRole('dialog', {name: /register form/i})
 })
